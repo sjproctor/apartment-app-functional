731345d2f9aa38f0a56e8db54637ab37d3b20356
Sarah Proctor <sproctor950@gmail.com>
2022-09-25 15:26:09 -0700

    :tada: initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,53 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+user = User.where(email: 'test@example.com').first_or_create(password: 'password', password_confirmation: 'password')
+
+apartment_seeds = [
+  {
+    street: '4 Privet Drive',
+    city: 'Little Whinging',
+    state: 'Surrey',
+    manager: 'Mr. Potter',
+    email: 'potter@example.com', 
+    price: 2000, 
+    bedrooms: 3, 
+    bathrooms: 2, 
+    pets: 'yes',
+    image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+  },
+  {
+    street: '15 Yemen Road',
+    city: 'Yemen',
+    state: 'Yemen',
+    manager: 'Mr. Bing',
+    email: 'bing@example.com', 
+    price: 1000, 
+    bedrooms: 3, 
+    bathrooms: 2, 
+    pets: 'yes',
+    image: 'https://i.pinimg.com/736x/4f/c1/ce/4fc1ce196ea1412f670d477a026ba2c6--saudi-arabia-drawing-reference.jpg'
+  },
+  {
+    street: '742 Evengrren Terrace',
+    city: 'Springfield',
+    state: 'Any State',
+    manager: 'Mr. Simpson',
+    email: 'simpson@example.com', 
+    price: 1000, 
+    bedrooms: 3, 
+    bathrooms: 2, 
+    pets: 'yes',
+    image: 'https://upload.wikimedia.org/wikipedia/en/c/ca/742_Evergreen_Terrace.png'
+  }
+]
+
+apartment_seeds.each do |apartment|
+  user.apartments.create(apartment)
+  p "creating: #{apartment}"
+end
